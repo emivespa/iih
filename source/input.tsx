503bc9f2873c2872e88327fdec1ea40703eccea4
Emilio Vesprini <emivespa1@gmail.com>
2023-06-11 23:12:34 +0000

    reformat date and usernames

--- source/input.tsx
+++ source/input.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
-import { Box, Text } from 'ink';
+import {Box, Text} from 'ink';
 
 import TextInput from 'ink-text-input';
 
@@ -8,38 +8,38 @@ import fs from 'fs';
 import path from 'path';
 
 type Props = {
-  host: string | undefined;
-  channel: string | undefined;
-  nick: string | undefined;
+	host: string | undefined;
+	channel: string | undefined;
+	nick: string | undefined;
 };
-export default function ChatInput({ host, channel, nick }: Props) {
-  // https://github.com/vadimdemedes/ink-text-input#usage
-  //
-  // TODO: use uncontrolled variant to prevent flicker?
-  // - uncontrolled variant seems to be WIP as of 20230604
-  const [query, setQuery] = useState('');
-  const handleSubmit = () => {
-    const HOME = process.env['HOME'];
-    const inFile = path.resolve(`${HOME}/irc/${host}/${channel}/in`);
-    fs.access(inFile, fs.constants.F_OK, err => {
-      if (err) {
-        console.error(`${inFile} does not exist`);
-        return;
-      }
-      const writeableStream = fs.createWriteStream(inFile, { flags: 'a' });
-      writeableStream.write(`${query}\n`);
-      writeableStream.end();
-    });
-    setQuery('');
-  };
-  return (
-    <Box width="100%">
-      <Box marginRight={1}>
-        <Text>&lt;{nick}&gt;</Text>
-      </Box>
-      <Box>
-        <TextInput value={query} onChange={setQuery} onSubmit={handleSubmit} />
-      </Box>
-    </Box>
-  );
+export default function ChatInput({host, channel}: Props) {
+	// https://github.com/vadimdemedes/ink-text-input#usage
+	//
+	// TODO: use uncontrolled variant to prevent flicker?
+	// - uncontrolled variant seems to be WIP as of 20230604
+	const [query, setQuery] = useState('');
+	const handleSubmit = () => {
+		const HOME = process.env['HOME'];
+		const inFile = path.resolve(`${HOME}/irc/${host}/${channel}/in`);
+		fs.access(inFile, fs.constants.F_OK, err => {
+			if (err) {
+				console.error(`${inFile} does not exist`);
+				return;
+			}
+			const writeableStream = fs.createWriteStream(inFile, {flags: 'a'});
+			writeableStream.write(`${query}\n`);
+			writeableStream.end();
+		});
+		setQuery('');
+	};
+	return (
+		<Box width="100%">
+			<Box marginRight={1}>
+				<Text>&gt;</Text>
+			</Box>
+			<Box>
+				<TextInput value={query} onChange={setQuery} onSubmit={handleSubmit} />
+			</Box>
+		</Box>
+	);
 }
--- source/line.tsx
+++ source/line.tsx
@@ -1,44 +1,53 @@
 import React from 'react';
 
-import {Text} from 'ink';
+import { Text } from 'ink';
 
-export default function Line({line}: {line: string}) {
-	const [unixDate, user, ...textTokens] = `${line}`.split(' ');
-	const text = textTokens.join(' ');
-	const time = new Date(Number(unixDate) * 1000).toLocaleTimeString();
-	// The userColor is a bit more complex. Let's get a random hex:
-	const userColor = ((s: string): string | undefined => {
-		// This isn't random so the distribution might be slightly biased.
-		const colors = [
-			// https://github.com/chalk/chalk#colors
-			/* 'black', */
-			'red',
-			'green',
-			'yellow',
-			'blue',
-			'magenta',
-			'cyan',
-			'white',
-			'blackBright',
-			'redBright',
-			'greenBright',
-			'yellowBright',
-			'blueBright',
-			'magentaBright',
-			'cyanBright',
-			/* 'whiteBright', */
-		];
-		const sum = s.split('').reduce((acc, i) => {
-			return acc + i.charCodeAt(0);
-		}, 0);
-		const i = sum % colors.length;
-		return colors[i];
-	})(user ? user : '0');
-	return (
-		<Text>
-			<Text dimColor>{time} </Text>
-			<Text color={userColor}>{user} </Text>
-			<Text>{text}</Text>
-		</Text>
-	);
+export default function Line({ line }: { line: string }) {
+  const [unixDate, user, ...textTokens] = `${line}`.split(' ');
+  const text = textTokens.join(' ');
+  const hh = new Date(Number(unixDate) * 1000)
+    .getHours()
+    .toString()
+    .padStart(2, '0');
+  const mm = new Date(Number(unixDate) * 1000)
+    .getMinutes()
+    .toString()
+    .padStart(2, '0');
+  const time = (hh + ':' + mm).padEnd(6);
+  const trimmedUser = user?.substring(0, 9).padStart(9).padEnd(10);
+  // The userColor is a bit more complex. Let's get a random hex:
+  const userColor = ((s: string): string | undefined => {
+    // This isn't random so the distribution might be slightly biased.
+    const colors = [
+      // https://github.com/chalk/chalk#colors
+      /* 'black', */
+      'red',
+      'green',
+      'yellow',
+      'blue',
+      'magenta',
+      'cyan',
+      'white',
+      'blackBright',
+      'redBright',
+      'greenBright',
+      'yellowBright',
+      'blueBright',
+      'magentaBright',
+      'cyanBright',
+      /* 'whiteBright', */
+    ];
+    const sum = s.split('').reduce((acc, i) => {
+      return acc + i.charCodeAt(0);
+    }, 0);
+    const i = sum % colors.length;
+    return colors[i];
+  })(user ? user : '0');
+  return (
+    <Text>
+      <Text color="blackBright">{time}</Text>
+      <Text color={userColor}>{trimmedUser}</Text>
+      <Text>{text}</Text>
+    </Text>
+  );
 }
