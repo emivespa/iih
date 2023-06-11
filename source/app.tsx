2461576e62c87e9f2829bc0fe9d9f990a91a0d31
Emilio Vesprini <emivespa1@gmail.com>
2023-06-11 20:59:50 +0000

    rework writes to fifo

--- source/app.tsx
+++ source/app.tsx
@@ -4,20 +4,16 @@ import ChatHistory from './history.js';
 import ChatInput from './input.js';
 
 type Props = {
-	host: string | undefined;
-	channel: string | undefined;
-	nick: string | undefined;
+  host: string | undefined;
+  channel: string | undefined;
+  nick: string | undefined;
 };
 
-export default function App({
-	host = 'irc.libera.chat',
-	channel = '#libera',
-	nick,
-}: Props) {
-	return (
-		<>
-			<ChatHistory host={host} channel={channel} nick={nick} />
-			<ChatInput host={host} channel={channel} nick={nick} />
-		</>
-	);
+export default function App({ host, channel, nick }: Props) {
+  return (
+    <>
+      <ChatHistory host={host} channel={channel} nick={nick} />
+      <ChatInput host={host} channel={channel} nick={nick} />
+    </>
+  );
 }
--- source/cli.tsx
+++ source/cli.tsx
@@ -1,84 +1,104 @@
 #!/usr/bin/env node
 import React from 'react';
-import { render } from 'ink';
+import {render} from 'ink';
 import meow from 'meow'; // https://github.com/sindresorhus/meow
 import App from './app.js';
-import { spawn } from 'child_process';
+import {execSync, spawn} from 'child_process';
 import path from 'path';
 import fs from 'fs';
 
 const cli = meow(
-  `
-	Usage
-	  $ iih --host irc.libera.chat --channel '##spam' --nick minime
+	`
+  Usage
+    $ iih --host irc.libera.chat --channel '##spam' --nick minime
 
-	Options
-	  --host     irc.libera.chat
-	  --channel  '##spam'
-	  --nick     minime
+  Options
+    --host     irc.libera.chat
+    --channel  '##spam'
+    --nick     minime
+    --pass     your-password # WARNING: will appear on processes list.
 
-	Examples
-	  $ iih --host irc.libera.chat --channel '##spam'
+  Examples
+    $ iih --host irc.libera.chat --channel '##spam' --nick minime --pass minipass
 `,
-  {
-    importMeta: import.meta,
-    flags: {
-      host: {
-        type: 'string',
-        shortFlag: 'h',
-        default: 'irc.libera.chat',
-      },
-      channel: {
-        type: 'string',
-        shortFlag: 'c',
-        default: '#libera',
-      },
-      nick: {
-        type: 'string',
-        shortFlag: 'n',
-        default: process.env['USER'],
-      },
-    },
-  },
+	{
+		importMeta: import.meta,
+		flags: {
+			host: {
+				type: 'string',
+				default: 'irc.libera.chat',
+			},
+			channel: {
+				type: 'string',
+				default: '#libera',
+			},
+			nick: {
+				type: 'string',
+				default: process.env['USER'],
+			},
+			pass: {
+				type: 'string',
+			},
+		},
+	},
 );
-
-// Start ii in the background:
 // TODO: don't if there are 'in' fifos present
-const iiProcess = spawn(
-  'ii',
-  ['-s', `${cli.flags.host}`, '-n', `${cli.flags.nick}`],
-  {
-    detached: true,
-    stdio: 'ignore',
-  },
-);
-iiProcess.unref();
-process.on('SIGINT', () => {
-  iiProcess.kill();
-  process.exit();
-});
+
+// Spawn ii in the background.
+
+const isIiRunning = () => {
+	try {
+		execSync('pidof ii');
+		return true;
+	} catch {
+		return false;
+	}
+};
+if (!isIiRunning()) {
+	const iiProcess = spawn(
+		'ii',
+		['-s', `${cli.flags.host}`, '-n', `${cli.flags.nick}`],
+		{
+			detached: true,
+			stdio: 'ignore',
+		},
+	);
+	iiProcess.unref();
+	process.on('SIGINT', () => {
+		iiProcess.kill();
+		process.exit();
+	});
+}
+
+const writeToFifo = (s: string, fifo: string) => {
+	const writeableStream = fs.createWriteStream(fifo, {flags: 'a'});
+	writeableStream.write(s);
+	writeableStream.end();
+};
 
 // Join send the /j(oin) message whenever the in file starts existing.
 const HOME = process.env['HOME'];
 const inFile = path.resolve(`${HOME}/irc/${cli.flags.host}/in`);
-const j = () => {
-  fs.access(inFile, fs.constants.F_OK, err => {
-    if (err) {
-      console.error(`${inFile} doesn't exist yet, waiting 1000ms...`);
-      setTimeout(j, 1000);
-      return;
-    }
-    const writeableStream = fs.createWriteStream(inFile, { flags: 'a' });
-    writeableStream.write(`/j ${cli.flags.channel}\n`);
-    writeableStream.end();
-    console.log('sent the /j(oin) msg');
-    render(
-      <App
-        channel={cli.flags.channel}
-        host={cli.flags.host}
-        nick={cli.flags.nick}
-      />,
-    );
-  });
+const writeOnceAvailable = () => {
+	fs.access(inFile, fs.constants.F_OK, err => {
+		if (err) {
+			console.error(`${inFile} doesn't exist yet, waiting 1000ms...`);
+			setTimeout(writeOnceAvailable, 1000);
+			return;
+		}
+		if (cli.flags.pass) {
+			const nick = `/m NickServ IDENTIFY ${cli.flags.nick} ${cli.flags.pass}\n`;
+			writeToFifo(nick, inFile);
+		}
+		const join = `/j ${cli.flags.channel}\n`;
+		writeToFifo(join, inFile);
+		render(
+			<App
+				channel={cli.flags.channel}
+				host={cli.flags.host}
+				nick={cli.flags.nick}
+			/>,
+		);
+	});
 };
-j();
+writeOnceAvailable();
